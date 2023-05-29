2eb3b47e07247ea88439be2616b16f35e2f6e12c
Sandeep Rai <sandeepksd@gmail.com>
2023-05-29 16:56:46 +0530

    Fix syntax in py code

--- lambda_function.py
+++ lambda_function.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
     print(ebody)
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo"
+        model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
@@ -23,9 +23,9 @@ def lambda_handler(event, context):
             },
             {
                 "role": "user",
-                "content": "<input>"
+                "content": ebody['payload']
             }
-        ]
+        ],
         temperature=1
     )
 
